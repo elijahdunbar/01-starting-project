4ed65c3f670a23dd94845b30b1b5505bfab97ed5
eli <eli.dunbar18@gmail.com>
2025-09-02 10:00:33 -0400

    restart

--- src/app/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  standalone: true,
-  styleUrl: './header.component.css',
-  templateUrl: './header.component.html',
-})
-export class HeaderComponent {}
